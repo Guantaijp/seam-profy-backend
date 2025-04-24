e68992d0f882b5994716efdf0be6435ee734ecd8
guantaijp <jpguantai@gmail.com>
2025-04-24 21:37:28 +0300

    :init

--- src/controllers/walletController.js
+++ src/controllers/walletController.js
@@ -117,7 +117,7 @@ export const topUpViaBank = async (req, res) => {
       });
     }
     
-    const wallet = await Wallet.findOne({ id: walletId, userId });
+    const wallet = await Wallet.findOne({ _id: walletId, userId });
     
     if (!wallet) {
       return res.status(404).json({
